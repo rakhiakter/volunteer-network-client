c47aff7d6285b705b5e8f83e18d9f50443f41685
Rakhi Akter <67531354+rakhiakter@users.noreply.github.com>
2020-10-04 22:49:42 +0600

    google form done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {  Container, Form, Image, Row } from 'react-bootstrap';
+import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            login here
-        </div>
+      <div>
+        <Container>
+          <Row id="create-logo">
+            <Image src={require("../../logos/Group 1329.png")} />
+          </Row>
+
+          <Row id="google">
+            <Form id="middle">
+              <h1 id="log">Login With</h1>
+              <br />
+              <Form.Group controlId="formBasicEmail">
+                <Form.Control type="email" placeholder="Continue with Google" />
+                <Form.Text className="text-muted">
+                  Don't have an Account? <a href="#">Create an account</a>
+                </Form.Text>
+              </Form.Group>
+            </Form>
+          </Row>
+        </Container>
+      </div>
     );
 };
 
