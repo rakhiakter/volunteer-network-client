bda980705dd2207e3f55a0b33d1890ebc6579b83
Rakhi Akter <67531354+rakhiakter@users.noreply.github.com>
2020-10-05 21:23:38 +0600

    add event api added

--- src/Components/CreateAccount/CreateAccount.js
+++ src/Components/CreateAccount/CreateAccount.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Container, Row, Form , Button, Image} from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import { UserContext } from '../../App';
 import './CreateAccount.css';
 const CreateAccount = (props) => {
-  const {event} = useParams();
-const {task} = props.location.state;
+  const [event, setEvent] = useState();
+  const registerButton = () => {
+console.log("submit");
+   fetch('http://localhost:5000/addEvent', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(event) 
+            })
+            .then(res => json())
+            .then(data => {
+                if(result){
+                    loadAllProducts();
+                     const update = document.getElementById('update');
+                    update.innerHTML = '';
+                }
+            })
+        }
+
+  };
+   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const {task} = useParams();
+const { eventName } = props.location.state;
 console.log(task);
     return (
       <div>
@@ -17,10 +38,10 @@ console.log(task);
             <Form id="register">
               <h3>Register as a Volunteer</h3>
               <Form.Group controlId="formBasicEmail">
-                <Form.Control type="text" placeholder="FullName" defaultValue={event} />
+                <Form.Control type="text" placeholder="FullName" defaultValue={loggedInUser.name} />
               </Form.Group>
               <Form.Group controlId="formBasicPassword">
-                <Form.Control type="text" placeholder="UserName Or Email" />
+                <Form.Control type="email" placeholder=" Email" defaultValue={loggedInUser.email}/>
               </Form.Group>
               <Form.Group controlId="formBasicPassword">
                 <Form.Control type="text" placeholder="Date" />
@@ -31,10 +52,10 @@ console.log(task);
               <Form.Group controlId="formBasicPassword">
                 <Form.Control
                   type="text"
-                  placeholder="Organize books at the library"
+                  placeholder="Organize books at the library" defaultValue={eventName}
                 />
               </Form.Group>
-              <Button variant="primary" size="lg">
+              <Button variant="primary" size="lg" onClick={registerButton}>
                 Registration
               </Button>{" "}
             </Form>
